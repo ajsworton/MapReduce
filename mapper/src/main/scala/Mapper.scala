918a6a38045add007ffbc209d44a0cadd2836dc0
Alexander Worton <alexander@ajsworton.com>
2019-06-21 10:06:34 +0100

    finished first pass with mapper and reducer

--- mapper/src/main/scala/Mapper.scala
+++ mapper/src/main/scala/Mapper.scala
@@ -7,10 +7,6 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    //    CustomerId  FirstName   LastName        Age   LTVEstimate ChurnScore  SegmentId
-    //    "59028934"  "Melanie"   "Gonzalez-Meza" "37"  "2798"      "0.273"     "8"
-
-    //get values
     val lines: Iterable[String] = EMR.lines
 
     val records: Iterable[Option[AcmeCustomerAttributes]] = lines.map { line =>
@@ -25,11 +21,12 @@ object Main {
     val filterAge: AcmeCustomerAttributes => Boolean = record => record.age >= 18 && record.age <= 32
     val filterChurn: AcmeCustomerAttributes => Boolean = record => record.churnScore >= 0.95
 
-    val filteredRecords: Iterable[AcmeCustomerAttributes] = records
+    val filteredRecordsSegmentId: Iterable[Int] = records
       .collect {
-        case Some(record) if filterAge(record) && filterChurn(record) => record
+        case Some(record) if filterAge(record) && filterChurn(record) => record.segmentId
       }
 
+    filteredRecordsSegmentId.foreach(println)
 
   }
 
--- reducer/src/main/scala/Reducer.scala
+++ reducer/src/main/scala/Reducer.scala
@@ -1,12 +1,18 @@
 package acme.reducer
 
-import scala.util.Try
-
 object Main {
 
   import acme.types.Imports._
   import acme.EMR
 
   def main(args: Array[String]): Unit = {
+
+    val groupIds: Iterable[String] = EMR.lines
+
+    groupIds
+      .groupBy(identity)
+      .map{ case (id, xs) => (id, xs.size) }
+      .foreach { case (groupId, number) => println(s"$groupId\t$number") }
+
   }
 }
